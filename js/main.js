3664f3d08b1935e8dd61be1035977341b05735c7
JesusOran <58339252+JesusOran@users.noreply.github.com>
2020-06-29 16:20:54 +0100

    updated gallery

--- js/main.js
+++ js/main.js
@@ -91,6 +91,10 @@ $("#relieve").click(function () {
 });
 
 function loadImages(max, galleryType) {
+  var galleryTitle = $(
+    '<div class="galleryTitle"><h1 class="mainTitle">Cerámica</h1><p class="galleryInfo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>'
+  );
+  mainContent.append(galleryTitle);
   var galleryCheck = galleryType;
   if (galleryType.includes("otros\\")) {
     galleryCheck = "otros";
