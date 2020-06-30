c84b6eb34efc862b6e248d5426407a1ebb3a7881
JesusOran <58339252+JesusOran@users.noreply.github.com>
2020-06-30 12:21:24 +0100

    scroll to top button

--- js/main.js
+++ js/main.js
@@ -153,3 +153,24 @@ function loadImages(max, galleryType) {
     //threshold:0
   });
 }
+
+var mybutton = $("#myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.css("display", "block");
+  } else {
+    mybutton.css("display", "none");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.click(function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0;
+}); // For Chrome, Firefox, IE and Opera
